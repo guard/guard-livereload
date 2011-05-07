86c2772ad03d4edb5259d7ac47dd790ade10a5ea
Tim McEwan <tim@mcewan.it>
2011-05-07 18:04:47 +1000

    support for :grace_period a la LiveReload

--- lib/guard/livereload.rb
+++ lib/guard/livereload.rb
@@ -19,7 +19,8 @@ module Guard
         :host => '0.0.0.0',
         :port => '35729',
         :apply_js_live => true,
-        :apply_css_live => true
+        :apply_css_live => true,
+        :grace_period => 0
       }.update(options)
     end
 
@@ -32,6 +33,7 @@ module Guard
     end
 
     def run_on_change(paths)
+      sleep @options[:grace_period]
       reactor.reload_browser(paths)
     end
 
